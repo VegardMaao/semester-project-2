d5238960a4fc214a1d392f7568955ae158e9686f
Vegard M. <vm932648@gmail.com>
2024-03-16 15:51:53 +0100

    Getting silly in the head and need a break. The moving main needs more work, I'll get to it later

--- src/js/visual-effects/loginEffects.mjs
+++ src/js/visual-effects/loginEffects.mjs
@@ -1,5 +1,6 @@
 export function changeForm(container) {
   const containerClass = container.classList;
+
   if (containerClass.contains("visible")) {
     container.style.cssText = "position: relative; top: -100vh;";
     container.classList.remove("visible");
