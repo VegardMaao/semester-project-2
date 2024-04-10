034975d3d929d3a2eec86fe228dd86ca13df603c
Vegard M. <vm932648@gmail.com>
2024-04-10 13:06:23 +0200

    improved sorting functions for all listings page

--- src/js/api-functions/get/getActions.mjs
+++ src/js/api-functions/get/getActions.mjs
@@ -1,3 +1,6 @@
+const currentDate = new Date().toJSON();
+let today = new Date(currentDate);
+
 /**
  *
  * @param {Array} postsArray
@@ -13,15 +16,27 @@ export function printFeed(domElement, postsArray) {
   domElement.innerHTML = "";
   const myUserName = localStorage.getItem("userName");
   for (let i = 0; i < postsArray.length; i++) {
-    const { title, description, media, id, _count, endsAt, tags } =
-      postsArray[i];
+    const {
+      title,
+      description,
+      media,
+      id,
+      _count,
+      endsAt,
+      tags,
+      bids,
+      seller,
+    } = postsArray[i];
+    const highestBid = bids[bids.length - 1];
     const endsDate = endsAt.replaceAll("-", ".");
     const slicedEndsDateAndTime = endsDate
       .slice(0, endsDate.length - 4)
       .split("T");
     const endDate = slicedEndsDateAndTime[0].split(".").reverse().join(".");
-    const endTime = slicedEndsDateAndTime[1];
-    domElement.innerHTML += `
+    const endTime = slicedEndsDateAndTime[1].replaceAll(".", "");
+
+    if (!highestBid) {
+      domElement.innerHTML += `
       <div class="single-listing">
       <a href="/pages/single-listing.html?${id}"><img
       src="${media}"
@@ -29,14 +44,66 @@ export function printFeed(domElement, postsArray) {
     <a href="/pages/single-listing.html?${id}">
       <h3 class="item-title">${title}</h3>
       </a>
+      <a href="/pages/profile.html?seller=${seller.name}"><p>By ${seller.name}</p></a>
       <p class="item-description">${description}</p>
-      <p>BIDS ${_count.bids}</p>
+      <p class="bids-amount">${_count.bids} bids</p>
       <p>Bidding ends the ${endDate} at ${endTime}</p>
       <a class="goto-bid" href="/pages/single-listing.html?${id}"><p>Make a bid</p></a>
       <div>
-        <p>tags: ${tags}</p>
-      </div>
+      <p>tags: ${tags}</p>
+    </div>
     </div>
             `;
+    } else {
+      domElement.innerHTML += `
+    <div class="single-listing">
+    <a href="/pages/single-listing.html?${id}"><img
+    src="${media}"
+  /></a>
+  <a href="/pages/single-listing.html?${id}">
+    <h3 class="item-title">${title}</h3>
+    </a>
+    <a href="/pages/profile.html?seller=${seller.name}"><p>By ${seller.name}</p></a>
+    <p class="item-description">${description}</p>
+    <p class="bids-amount">${_count.bids} bids, last bid at ${highestBid.amount}</p>
+    <p>Bidding ends the ${endDate} at ${endTime}</p>
+    <a class="goto-bid" href="/pages/single-listing.html?${id}"><p>Make a bid</p></a>
+    <div>
+      <p>tags: ${tags}</p>
+    </div>
+  </div>
+          `;
+    }
+  }
+}
+
+/**
+ * Sorts an array before creating HTML
+ * @param {array} postsArray - array of posts from the API
+ * @param {object} domElement - where in the DOM the new array is printed
+ * @param {string} sortBy - how to sort the array
+ * This function sorts my array before printing out the feed in a new order.
+ */
+export function sortArray(domElement, postsArray, sortBy) {
+  switch (sortBy) {
+    case "most-bids":
+      const arrayByMostBids = postsArray.sort(function (x, y) {
+        return y._count.bids - x._count.bids;
+      });
+      printFeed(domElement, arrayByMostBids);
+      break;
+
+    case "least-bids":
+      const arrayByLeastBids = postsArray
+        .sort(function (x, y) {
+          return y._count.bids - x._count.bids;
+        })
+        .slice()
+        .reverse();
+      printFeed(domElement, arrayByLeastBids);
+      break;
+
+    default:
+      break;
   }
 }
--- src/js/pagesJS/allListings.mjs
+++ src/js/pagesJS/allListings.mjs
@@ -7,7 +7,54 @@ let completeUrl;
 
 const allListingsDOM = document.querySelector(".all-listings");
 
-endpoint = "/auction/listings";
+endpoint = "/auction/listings?_seller=true&_bids=true";
 completeUrl = baseUrl + endpoint;
 
 getData.getData(completeUrl, allListingsDOM, getActions.printFeed);
+
+const searchOptions = document.querySelector(".options");
+const searchInp = document.querySelector("#site-search");
+const searchBy = document.querySelector("#search-keys");
+const sortingInp = document.querySelector("#sort-by");
+
+searchOptions.onsubmit = (e) => {
+  e.preventDefault();
+};
+
+sortingInp.addEventListener("change", (e) => {
+  console.log(sortingInp.value);
+  allListingsDOM.innerHTML = "";
+
+  switch (sortingInp.value) {
+    case "default":
+      completeUrl = `${baseUrl}${endpoint}`;
+      getData.getData(completeUrl, allListingsDOM, getActions.printFeed);
+      break;
+    case "newest-bids":
+      completeUrl = `${baseUrl}${endpoint}&sort=created&sortOrder=desc`;
+      getData.getData(completeUrl, allListingsDOM, getActions.printFeed);
+      break;
+    case "oldest-bids":
+      completeUrl = `${baseUrl}${endpoint}&sort=created&sortOrder=asc`;
+      getData.getData(completeUrl, allListingsDOM, getActions.printFeed);
+      break;
+    case "most-bids":
+      getData.getData(
+        completeUrl,
+        allListingsDOM,
+        getActions.sortArray,
+        sortingInp.value
+      );
+      break;
+    case "least-bids":
+      getData.getData(
+        completeUrl,
+        allListingsDOM,
+        getActions.sortArray,
+        sortingInp.value
+      );
+      break;
+    default:
+      break;
+  }
+});
